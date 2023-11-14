b1869c445444e76ab94605f1f68437d9cac6fe3d
Felipe <78279691+felipe29cl@users.noreply.github.com>
2023-11-14 01:03:33 +0000

    Proyecto Streamlit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,16 @@
-from utils import db_connect
-engine = db_connect()
+import streamlit as st
+import joblib
 
-# your code here
+clf = joblib.load('../models/modelo_de_clasificacion.pkl')
+vectorizer = joblib.load('../models/vectorizador.pkl')
+
+st.title("Clasificador de productos")
+
+user_input = st.text_input("Ingresa el nombre de producto del Supermercado para clasificarlo. (Ej: Papas fritas marco polo 230 gramos)")
+
+if user_input:
+    user_input_vectorized = vectorizer.transform([user_input])
+
+    prediction = clf.predict(user_input_vectorized)
+
+    st.write("El producto pertenece a la categoría:", prediction[0])
